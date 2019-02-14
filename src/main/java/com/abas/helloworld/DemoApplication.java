09d473e87212b110c7a39f63b1355ecc4d954609
abasjr <anggitbaskoro7@gmail.com>
2019-02-15 00:19:56 +0700

    new project

---
+++ src/main/java/com/abas/helloworld/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.abas.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	@GetMapping(value = "/hello")
+	public String getMethodName() {
+		return "Hello";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
